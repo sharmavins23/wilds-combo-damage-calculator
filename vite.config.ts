d722e18e7cac356f5b01c8549c16fa3d831d9b4f
sharmavins23 <sharmavins23@gmail.com>
2025-03-11 20:39:10 -0700

    Updated deployment with base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/wilds-combo-damage-calculator/",
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
